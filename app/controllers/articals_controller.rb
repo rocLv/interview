6648e54f21ce68d47d31d985ae5f04b77c06e184
rocLv <wangqsh999@gmail.com>
2016-05-23 21:54:15 +0800

    add articals

--- app/controllers/articals_controller.rb
+++ app/controllers/articals_controller.rb
@@ -1,2 +1,14 @@
 class ArticalsController < ApplicationController
+  def index
+    @articals = Artical.all
+  end
+
+  def show
+    @artical = Artical.find(params[:id])
+    @comments = @artical.comments
+    unless @comments
+      @comments = []
+      @comments = @artical.comments.build
+    end
+  end
 end
